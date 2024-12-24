652e047d274ecbecaac62158c7ca5e416d7b3f52
Bilal202202001 <muhammad.bilal.94390@gmail.com>
2024-12-24 11:39:49 +0500

    Update-Deployement

--- server/index.js
+++ server/index.js
@@ -1,35 +1,66 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import cookieParser from 'cookie-parser';
+// import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+// import authRouter from "./routes/authRoutes.js";
+// import videoRoutes from './routes/video.routes.js'
+// dotenv.config();
+
+// const app = express();
+// mongoose.connect(process.env.MONGODB_URL);
+// app.use(cors({
+//   origin: [process.env.FRONTEND_URL],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   credentials: true,
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+
+// app.use(cookieParser());
+// app.use(bodyParser.json());
+// app.use('/auth', authRouter);
+// app.use('/video', videoRoutes)
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// const PORT = 5001;
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
+
+
+import mongoose from "mongoose";
+import express from "express";
+import cookieParser from "cookie-parser";
 import cors from 'cors';
-import authRouter from "./routes/authRoutes.js";
-import videoRoutes from './routes/video.routes.js'
-dotenv.config();
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL);
+app.use(express.json())
 app.use(cors({
-  origin: [process.env.FRONTEND_URL],
+  origin: ['https://solestyle.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }));
 
 
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use('/auth', authRouter);
-app.use('/video', videoRoutes)
+app.use(express.static('public'))
+app.use(cookieParser())
 
+mongoose.connect('mongodb+srv://muhammadbilal94390:bilalkhan94390@cluster0.ibfi1yh.mongodb.net/testEcom?retryWrites=true&w=majority&appName=Cluster0');
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
-const PORT = 5001;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+
+
+app.listen(3001, () => {
+  console.log("Server is Running")
+})
+
+
 
 
